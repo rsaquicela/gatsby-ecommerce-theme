a1faf9d5ccab588598f1716521de56f8df820cc3
rsaquicela <92008717+rsaquicela@users.noreply.github.com>
2023-03-07 00:06:35 -0500

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -13,7 +13,7 @@ const Brand = (props) => {
       {/* <h4>SYDNEY</h4> */}
       <svg
         width={127}
-        height={24}
+        height={800px}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 127.67 23.36"
       >
