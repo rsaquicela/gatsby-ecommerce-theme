fb6851a8b30a38e88e34fc1e77645f93f01aada1
rsaquicela <92008717+rsaquicela@users.noreply.github.com>
2023-03-07 02:51:42 -0500

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -12,7 +12,7 @@ const Brand = (props) => {
     >
       {/* <h4>made with love</h4> */}
       <svg
-        width={164}
+        width={184}
         height={64}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="-32 -32 128.00 128.00"
